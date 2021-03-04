9bce634a9d22b333bc83c2d8adbc3e1604f9ece9
AnnSo <64138186+auAnnSo@users.noreply.github.com>
2021-03-04 13:57:51 +0100

    Started IChargeControl

---
+++ Charger-Functionality-Library/IChargeControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Charger_Functionality_Library
+{
+    public class IChargeControl
+    {
+        public bool isConnected();
+
+
+    }
+}
