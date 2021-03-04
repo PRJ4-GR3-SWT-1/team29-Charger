ca9dbc844c526cb5bf32bab3045533e804926d52
Laurits H.G <201905745@post.au.dk>
2021-03-04 14:01:21 +0100

    Prototype Door Interface added

---
+++ Charger-Functionality-Library/IDoor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Charger_Functionality_Library
+{
+    interface IDoor
+    {
+        bool locked { get; set; }
+        void lockDoor();
+        void unlockDoor();
+        void setState(bool state);
+        void initialise();
+    }
+}
