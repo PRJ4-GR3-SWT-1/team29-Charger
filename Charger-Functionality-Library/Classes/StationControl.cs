99162262842b520042bcc0c9644299dc51862e95
Laurits H.G <201905745@post.au.dk>
2021-03-11 13:25:13 +0100

    started Stationcontrol

--- Charger-Functionality-Library/Classes/StationControl.cs
+++ Charger-Functionality-Library/Classes/StationControl.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using Charger_Functionality_Library.Interfaces;
 
 namespace Charger_Functionality_Library
 {
     public class StationControl
     {
+        private IDoor door;
+        private IRFIDReader rfidReader;
+        private IDisplay display;
+        private IChargeControl chargeControl;
+        private ILogFile logFile;
+
+        public StationControl(IDoor Do, IRFIDReader RFID,IDisplay Dis, IChargeControl Charge,ILogFile log)
+        {
+            door = Do;
+            rfidReader = RFID;
+            display = Dis;
+        }
         public void DoorOpened()
         {
             throw new System.NotImplementedException();
