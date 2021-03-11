4808298e08cc8318bf9e9f3e532fcd7226a06656
AnnSo <64138186+auAnnSo@users.noreply.github.com>
2021-03-11 14:00:31 +0100

    Initial work on Door started

--- Charger-Functionality-Library/Classes/Door.cs
+++ Charger-Functionality-Library/Classes/Door.cs
@@ -1,11 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Charger_Functionality_Library.EventArgsClasses;
 using Charger_Functionality_Library.Interfaces;
 
 namespace Charger_Functionality_Library.Classes
 {
     public class Door: IDoor
     {
+        private bool locked = true; 
+        public bool Locked
+        {
+            get => locked;
+            set => locked = Locked;
+        }
+
+        public bool open = true;
+        public bool Open
+        {
+            get => open;
+            set => open = Open;
+        }
+
+        public event EventHandler<DoorEventArgs> DoorOpenEvent;
+        public event EventHandler<DoorEventArgs> DoorCloseEvent;
+
+        public void LockDoor()
+        {
+            Locked = true;
+        }
+
+        public void UnlockDoor()
+        {
+            Locked = false;
+        }
+
+        public void OpenDoor()
+        {
+            if (Locked)
+            {
+                Open = true;
+                OnDoorOpen(new DoorEventArgs{});
+            }
+        }
+
+        protected virtual void OnDoorOpen(DoorEventArgs e)
+        {
+            DoorOpenEvent?.Invoke(this,e);
+        }
+
+        public void CloseDoor()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Charger-Functionality-Library/EventArgsClasses/DoorEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Charger_Functionality_Library.EventArgsClasses
+{
+    public class DoorEventArgs:EventArgs
+    {
+
+    }
+}
