ebb639f75c6701263542f0d145a792df8f617178
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-03-11 16:44:06 +0100

    Jeg burde researche praktik, men i stedet er jeg begyndt med at teste Door :O

--- Charger-Functionality-Library/Classes/ChargeControl.cs
+++ Charger-Functionality-Library/Classes/ChargeControl.cs
@@ -7,5 +7,19 @@ namespace Charger_Functionality_Library.Classes
 {
     public class ChargeControl: IChargeControl
     {
+        public bool IsConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopCharge()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Ladeskab-Unit-Test/UnitTest1.cs
+++ Ladeskab-Unit-Test/UnitTest1.cs
@@ -1,18 +1,42 @@
+using System.Xml.Linq;
+using Charger_Functionality_Library.Classes;
+using Charger_Functionality_Library.Interfaces;
 using NUnit.Framework;
 
 namespace Ladeskab_Unit_Test
 {
-    public class Tests
+    public class DoorTests
     {
+        private Door door;
         [SetUp]
         public void Setup()
         {
+            
+            door = new Door();
+            door.DoorOpenEvent += Door_DoorOpenEvent;
+            numberOfEvents = 0;
         }
 
-        [Test]
-        public void Test1()
+        private void Door_DoorOpenEvent(object sender, Charger_Functionality_Library.EventArgsClasses.DoorEventArgs e)
         {
-            Assert.Pass();
+            numberOfEvents++;
+        }
+
+        public int numberOfEvents { get; set; }
+
+        [Test]
+        public void OpenDoor_DoorIsUnlocked_DoorIsOpen()
+        {
+            door.UnlockDoor();
+            door.OpenDoor();
+            Assert.That(door.open,Is.EqualTo(true));
+        }
+        [Test]
+        public void OpenDoor_DoorIsUnlocked_EventIsTriggered()
+        {
+            door.UnlockDoor();
+            door.OpenDoor();
+            Assert.That(numberOfEvents, Is.EqualTo(1));
         }
     }
 }
