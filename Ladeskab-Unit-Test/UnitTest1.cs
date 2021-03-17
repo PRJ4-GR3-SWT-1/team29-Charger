4e5abb4cf730cbc5529347d7e5bea36434beb7ce
Laurits H.G <201905745@post.au.dk>
2021-03-17 09:04:01 +0100

    Added UnitTests for Display

--- Ladeskab-Unit-Test/UnitTest1.cs
+++ Ladeskab-Unit-Test/UnitTest1.cs
@@ -1,6 +1,8 @@
 using System.Xml.Linq;
 using Charger_Functionality_Library.Classes;
+using Charger_Functionality_Library.Help_Interfaces;
 using Charger_Functionality_Library.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Ladeskab_Unit_Test
@@ -37,6 +39,21 @@ namespace Ladeskab_Unit_Test
             door.UnlockDoor();
             door.OpenDoor();
             Assert.That(numberOfEvents, Is.EqualTo(1));
+        }
+        // ----- UnitTests for Display -----
+        [Test]
+        public void Display_MethodIsCalled_DisplayUsesInterface()
+        {
+            IConsoleWriter _writer = Substitute.For<IConsoleWriter>();
+            Display display = new Display(_writer);
+
+            display.CabinetOccupied();
+            display.RFIDError();
+            display.ConnectionError();
+            display.PhoneConnected();
+            display.RFIDRead();
+            display.RemovePhone();
+            _writer.Received(6).write(Arg.Any<string>());
         }
     }
 }
