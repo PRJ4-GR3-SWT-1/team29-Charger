f6ba1fe3c11f722414bebba55e616486c3748ade
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-03-24 09:19:41 +0100

    arbejdet på console app

--- Charger-Functionality-Library/Help_Interfaces/IFileWriter.cs
+++ Charger-Functionality-Library/Help_Interfaces/IFileWriter.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Charger_Functionality_Library.Help_Interfaces
 {
     public interface IFileWriter
     {
-        string[] File { get; set; }
+        
 
+        public void WriteToFile(string txt);
+    }
+
+    public class FileWriter : IFileWriter
+    {
+        
         public void WriteToFile(string txt)
         {
-
+            using (StreamWriter w = File.AppendText("LadeskabLog.txt"))
+            {
+                w.Write(txt);
+            }
         }
     }
 }
--- runnable-console-app/Program.cs
+++ runnable-console-app/Program.cs
@@ -1,25 +1,36 @@
 ﻿using System;
 using Charger_Functionality_Library;
 using Charger_Functionality_Library.Classes;
+using Charger_Functionality_Library.Help_Interfaces;
+using Charger_Functionality_Library.Interfaces;
+using UsbSimulator;
 
 namespace runnable_console_app
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            // Assemble your system here from all the classes
-            Door door = new Door(); //Der bruges metoder (open og close) der ikke er en del af interface
-           
-
             bool finish = false;
+
+            //Assemble your system here from all the classes
+            Door door = new Door(); //Der bruges metoder (open og close) der ikke er en del af interface
+            IDisplay display = new Display(new ConsoleWriter());
+            IUsbCharger usb = new UsbChargerSimulator();
+            ChargeControl chargeControl = new ChargeControl(usb, display);
+            ILogFile logFile = new LogFile(new TimeProvider(), new FileWriter());
+            RfidReader rfidReader = new RfidReader();
+            door = new Door();
+            
+            StationControl station = new StationControl(door, rfidReader, display, chargeControl, logFile);
             do
             {
                 string input;
-                System.Console.WriteLine("Indtast E, O, C, R: ");
+                System.Console.WriteLine("Indtast bogstav: E_nd, O_pen, C_lose, R_rfid, P_lug eller U_nplug: ");
                 input = Console.ReadLine();
                 if (string.IsNullOrEmpty(input)) continue;
-
+                input = input.ToUpper();
                 switch (input[0])
                 {
                     case 'E':
@@ -34,13 +45,18 @@ namespace runnable_console_app
                         door.CloseDoor();
                         break;
 
-                    //case 'R':
-                    //    System.Console.WriteLine("Indtast RFID id: ");
-                    //    string idString = System.Console.ReadLine();
-
-                    //    int id = Convert.ToInt32(idString);
-                    //    rfidReader.OnRfidRead(id);
-                    //    break;
+                    case 'R':
+                        System.Console.WriteLine("Indtast RFID id: ");
+                        string idString = System.Console.ReadLine();
+                        
+                        rfidReader.ManualScanTag(idString);
+                        break;
+                    case 'P':
+                        chargeControl.PlugPhoneIn();
+                        break;
+                    case 'U':
+                        chargeControl.UnPlugPhone();
+                        break;
 
                     default:
                         break;
